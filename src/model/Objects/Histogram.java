3db34016c9099a8a8ee662a50818d193bb1418f4
JovenCruz <jovencruz23@gmail.com>
2014-10-08 18:47:40 +0800

    Histogram and Segmentation Changes
    Attempt to emulate Atomica Structure for Histogram and Segmentation
    >> Personally require to update FFMPEG
    >> Not yet tested -- almost there folks.

--- src/model/Objects/Histogram.java
+++ src/model/Objects/Histogram.java
@@ -2,11 +2,35 @@ package model.Objects;
 
 public class Histogram 
 {
+	public int[] getRED() {
+		return RED;
+	}
+
+	public void setRED(int[] r) {
+		RED = r;
+	}
+
+	public int[] getGREEN() {
+		return GREEN;
+	}
+
+	public void setGREEN(int[] g) {
+		GREEN = g;
+	}
+
+	public int[] getBLUE() {
+		return BLUE;
+	}
+
+	public void setBLUE(int[] b) {
+		BLUE = b;
+	}
+
 	private int[] RED = new int[16];
 	private int[] GREEN = new int[16];
 	private int[] BLUE = new int[16];
 	
-	public Histogram ()
+	public Histogram()
 	{
 		for (int i = 0; i < 16; i++)
         {
--- src/model/Segmentation.java
+++ src/model/Segmentation.java
@@ -5,90 +5,145 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+import model.Objects.Histogram;
+
 public class Segmentation 
 {
+	
+	private int BINS = 16;
 	private String filepath;
 	
 	public Segmentation(String absolutePath) 
 	{
 		this.filepath = absolutePath;							
 	}
+	
+	public Histogram findHistogram(File image)
+	{
+		Histogram currentHist = new Histogram();
+		BufferedImage buffImage = null;
+		
+		int[] tempRed = new int[16];
+		int[] tempGreen = new int[16];
+		int[] tempBlue = new int[16];
+       
+		try 
+		{
+        	buffImage = ImageIO.read(image);
+		} catch (IOException e1) 
+		{
+			e1.printStackTrace();
+		}
+		
+		int width = buffImage.getWidth();
+		int height = buffImage.getHeight();			
+				
+    	for(int y = 0; y < height; y++)
+    	{
+    		for(int x = 0; x < width; x++)
+    		{
+    			
+    			int currentPixel = buffImage.getRGB(x, y);
+    			
+    			
+    			int red = (currentPixel >> 16) & 0xff;
+    			int green = (currentPixel >> 8) & 0xff;
+    			int blue = (currentPixel) & 0xff;
+    			
+                tempRed[red / BINS]++;             //gets red values
+                tempGreen[green / BINS]++;             //gets green values
+                tempBlue[blue / BINS]++;             //gets blue values
+    			
+                currentHist.setRED(tempRed);
+                currentHist.setGREEN(tempGreen);
+                currentHist.setBLUE(tempBlue);
+                
+		    	//System.out.println("Results: [Red: "+red+"] || [Green: "+green+"] || [Blue: "+blue+"]");
+    		    			
+    		}
+    	}
+		
+		return currentHist;
+		
+	}
+	
+	public double euclideanDist(Histogram Ha, Histogram Hb)
+	{
+		double eucDist = 0;
+		
+        for (int i = 0; i < BINS; i++)
+        {
+            //EUCLIDEAN DISTANCE
+           eucDist += Math.sqrt(Math.pow(Ha.getRED()[i] - Hb.getRED()[i], 2) + Math.pow(Ha.getGREEN()[i] - Hb.getGREEN()[i], 2) + Math.pow(Ha.getBLUE()[i] - Hb.getBLUE()[i], 2));
+
+        }
+        return eucDist;
+    }
+	
+	
 	public void segmentMovie()
 	{
 		File f = new File(filepath);							
-		int imageCount = f.listFiles().length;		
+		int imageCount = f.listFiles().length;	
+		int counterImage = 0; //to let us know how many images in one segment
+		double distance_threshold = 89908.84; //experimental threshold
+		int fileEnd = imageCount;
+		
+		Histogram histA;
+		Histogram histB;
+		Histogram histCheck;
 		
 		for(int i = 1; i < imageCount - 1; i++)
 		{
+			
+			double imageDiff = 0;
+			
 			String imagePath1 = filepath + "\\" + i + ".jpeg";
 			String imagePath2 = filepath + "\\" + (i+1) + ".jpeg";
 			
 			File image1 = new File(imagePath1);
 			File image2 = new File(imagePath2);
-	
-			BufferedImage img1 = null;
-	        BufferedImage img2 = null;	        	        
-	        
-	        try 
-	        {
-	        	img1 = ImageIO.read(image1);
-				img2 = ImageIO.read(image2);
-								
-			} catch (IOException e1) 
-			{
-				e1.printStackTrace();
-			}
-	        
-	        
-	        
-	        int pixc = 0;
-	        
-		
-			int width1 = img1.getWidth();
-			int height1 = img2.getHeight();			
-					
-	    	for(int y = 0; y < height1; y++)
-	    	{
-	    		for(int x = 0; x < width1; x++)
-	    		{
-	    			
-	    			int pix1 = img1.getRGB(x, y);
-	    			
-	    			pixc++;
-	    			
-	    			int red = (pix1 >> 16) & 0xff;
-	    			int green = (pix1 >> 8) & 0xff;
-	    			int blue = (pix1) & 0xff;
-	    			
-			    	System.out.println("Pixel "+pixc+" Frame "+i+" Results: [Red: "+red+"] || [Green: "+green+"] || [Blue: "+blue+"]");
-	    		    			
-	    		}
-	    	}
-	    	
-	    	
-			int width2 = img2.getWidth();
-			int height2 = img2.getHeight();
+			
+			histA = findHistogram(image1);
+			histB = findHistogram(image2);
+			imageDiff = euclideanDist(histA, histB);
+			
+            if (imageDiff < distance_threshold)
+            {
+               counterImage++;
+            }
+            
+            else //not similar, second change
+            {
+                if (i + 2 == fileEnd) break;
 
-	    	pixc = 0;
-	    	
-	    	for(int y = 0; y < height2; y++)
-	    	{
-	    		for(int x = 0; x < width2; x++)
-	    		{
-	    			
-	    			int pix2 = img2.getRGB(x, y);
-	    			
-	    			pixc++;
-	    			
-	    			int red = (pix2 >> 16) & 0xff;
-	    			int green = (pix2 >> 8) & 0xff;
-	    			int blue = (pix2) & 0xff;
-	    			
-			    	System.out.println("Pixel "+pixc+" Frame "+(i+1)+" Results: [Red: "+red+"] || [Green: "+green+"] || [Blue: "+blue+"]");
-	    		    			
-	    		}
-	    	}
-	        
+                //Image A and B are not the same; Compare Image A and Image Check
+                try
+                {
+                	String imagePath3 = filepath + "\\" + (i+2) + ".jpeg";
+                	File image3 = new File(imagePath3);
+                    histCheck = findHistogram(image3);
+                    imageDiff = euclideanDist(histA, histCheck);
+                }
+                catch (Exception e2) 
+                {
+                	e2.printStackTrace();
+                }
+                // Image A and Image Check are the same.
+                if (imageDiff < distance_threshold)
+                {
+                    counterImage++;
+                }
+                else
+                {
+                    if (counterImage > 4)
+                    {
+                        counterImage = 0;
+                    }
+                }
+            }
+	
 		}
 	}
+
 }
