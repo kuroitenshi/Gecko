1cb76555070e2690d0562de117d0e3674989f882
kuroitenshi <phibo_pg@yahoo.com>
2014-10-16 19:03:37 +0800

    Acquired RGB values for Visual Disturbance

--- src/model/Frame.java
+++ src/model/Frame.java
@@ -4,21 +4,17 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import javax.imageio.ImageIO;
-
-import model.Objects.Histogram;
 
 public class Frame
 {
 	private BufferedImage image;
 	private int key;
-	private ArrayList<RGB> rgb;
-	private String directory;
+	private ArrayList<RGB> rgb;	
 	
 	public Frame(int key, String directory)
 	{
-		this.directory = directory;
+		
 		File file = new File(directory);
 	
 		try 
@@ -28,35 +24,11 @@ public class Frame
 			e.printStackTrace();
 		}
 		
-		setKey(key);
+		this.setKey(key);
 		rgb = new ArrayList<RGB>();
-		computeRGB();
+		this.computeRGB();
 	}
 
-	public BufferedImage getImage() 
-	{
-		return image;
-	}
-
-	public void setImage(BufferedImage image) 
-	{
-		this.image = image;
-	}
-
-	public int getKey() 
-	{
-		return key;
-	}
-
-	public void setKey(int key) 
-	{
-		this.key = key;
-	}
-
-	public RGB getRgb(int index) 
-	{
-		return rgb.get(index);
-	}
 
 	private void computeRGB() 
 	{
@@ -99,49 +71,84 @@ public class Frame
 		{
 			for(int j = 0; j < width; j++)
 			{
+				int currentPixel = image.getRGB(i, j);
+				int red = (currentPixel >> 16) & 0xff;
+				int green = (currentPixel >> 8) & 0xff;
+    			int blue = (currentPixel) & 0xff;
+				
 				try
 				{
 					// first block
 					if(row1hs <= i && i <= row1he && col1ws <= j && j <= col1we)
-					{
-//						  data1.r += p[0];             //gets red values
-//                          data1.g += p[1];             //gets green values
-//                          data1.b += p[2];             //gets blue values
+					{						
+		    			rgb1.setR(red);
+						rgb1.setG(green);
+						rgb1.setB(blue);
+
 					} 
 					// second block
 					else if(row1hs <= i && i <= row1he && col2ws <= j && j<= col2we)
 					{
-						
+						rgb2.setR(red);
+						rgb2.setG(green);
+						rgb2.setB(blue);
+
 					}
 					// third block
 					else if(row1hs <= i && i <= row1he && col3ws <= j && j<= col3we)
 					{
-						
+						rgb3.setR(red);
+						rgb3.setG(green);
+						rgb3.setB(blue);
+
 					}
 					// fourth block
 					else if(row2hs <= i && i <= row2he && col1ws <= j && j <= col1we)
 					{
-						
+						rgb4.setR(red);
+						rgb4.setG(green);
+						rgb4.setB(blue);
+
 					}
 					//5th block
 					else if (row2hs <= i && i <= row2he && col2ws <= j && j <= col2we)
 					{
+						rgb5.setR(red);
+						rgb5.setG(green);
+						rgb5.setB(blue);
+
 					}
 					//6th block
 					else if (row2hs <= i && i <= row2he && col3ws <= j && j <= col3we)
 					{
+						rgb6.setR(red);
+						rgb6.setG(green);
+						rgb6.setB(blue);
+
 					}
 					//7th block
 					else if (row3hs <= i && i <= row3he && col1ws <= j && j <= col1we)
 					{
+						rgb7.setR(red);
+						rgb7.setG(green);
+						rgb7.setB(blue);
+
 	                }
 					//8th block
 					else if (row3hs <= i && i <= row3he && col2ws <= j && j <= col2we)
 					{
+						rgb8.setR(red);
+						rgb8.setG(green);
+						rgb8.setB(blue);
+
 	                }
 					//9th block
 					else if (row3hs <= i && i <= row3he && col3ws <= j && j <= col3we)
 					{
+						rgb9.setR(red);
+						rgb9.setG(green);
+						rgb9.setB(blue);
+
 	                }
 				}
 				catch(Exception ex)
@@ -162,5 +169,31 @@ public class Frame
 		rgb.add(rgb7);
 		rgb.add(rgb8);
 		rgb.add(rgb9);
+	}
+	
+
+	public BufferedImage getImage() 
+	{
+		return image;
+	}
+
+	public void setImage(BufferedImage image) 
+	{
+		this.image = image;
+	}
+
+	public int getKey() 
+	{
+		return key;
+	}
+
+	public void setKey(int key) 
+	{
+		this.key = key;
+	}
+
+	public RGB getRgb(int index) 
+	{
+		return rgb.get(index);
 	}
 }
--- src/model/Shot.java
+++ src/model/Shot.java
@@ -1,22 +1,19 @@
 package model;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class Shot 
 {
 	private int key;
 	private ArrayList<Frame> frames;
+	private String framesPath;
 	private double visualDisturbanceValue;
 	
-	public int getKey() 
+	public Shot(String framesPath)
 	{
-		return key;
-	}
-
-	public void setKey(int key) 
-	{
-		this.key = key;
-	}
+		this.framesPath = framesPath;
+	} 	
 
 	public Shot(int key)
 	{
@@ -27,23 +24,17 @@ public class Shot
 	}
 	
 	private void getFrames(int key) 
-	{
-		//retrieve frames via file
-		//store in arraylist frames
-//		st = str.Split(' ');
-//        start = Convert.ToInt32(st[3]);
-//        end = Convert.ToInt32(st[4]);
+	{		
+		File framesFile = new File(this.framesPath);
+		int framesFileSize = framesFile.listFiles().length;
+		for(int i = 1; i < framesFileSize-1; i++)
+		{
+			Frame retrievedFrame = new Frame(i, this.framesPath + "\\" + i + ".jpeg" );
+			this.frames.add(retrievedFrame);			
+		}
+				
 	}	
 	
-	public double getVisualDisturbanceValue() 
-	{
-		return visualDisturbanceValue;
-	}
-
-	public void setVisualDisturbanceValue(double visualDisturbanceValue) 
-	{
-		this.visualDisturbanceValue = visualDisturbanceValue;
-	}
 	
 	public void computeVisualDisturbance()
 	{
@@ -51,19 +42,19 @@ public class Shot
 		int counter = 0;
 		int temp = 0;
 		int divisor = 0;
-		int frameInterval = frames.get(frames.size()-1).getKey() - frames.get(0).getKey();
+		int frameInterval = this.frames.get(frames.size()-1).getKey() - this.frames.get(0).getKey();
 		
-		for(int i = 1; i < frames.size(); i++)
+		for(int i = 1; i < this.frames.size(); i++)
 		{
 			for (int j = 0; j < 9; j++)
 			{
-				double r1 = frames.get(i).getRgb(j).getR();
-				double g1 = frames.get(i).getRgb(j).getG();
-				double b1 = frames.get(i).getRgb(j).getB();
+				double r1 = this.frames.get(i).getRgb(j).getR();
+				double g1 = this.frames.get(i).getRgb(j).getG();
+				double b1 = this.frames.get(i).getRgb(j).getB();
 				
-				double r2 = frames.get(i-1).getRgb(j).getR();
-				double g2 = frames.get(i-1).getRgb(j).getG();
-				double b2 = frames.get(i-1).getRgb(j).getB();
+				double r2 = this.frames.get(i-1).getRgb(j).getR();
+				double g2 = this.frames.get(i-1).getRgb(j).getG();
+				double b2 = this.frames.get(i-1).getRgb(j).getB();
 				
 				double distance = Math.abs(r1 - r2) + Math.abs(g1 - g2) + Math.abs(b1 - b2);
 				double max = Math.max(r1, r2) + Math.max(g1, g2) + Math.max(b1, b2);
@@ -88,4 +79,27 @@ public class Shot
 		divisor = 9 * (frameInterval) / 2;
 		visualDisturbanceValue = (counter * 1.0 / divisor);
 	}	
+	
+	public int getKey() 
+	{
+		return key;
+	}
+
+	public void setKey(int key) 
+	{
+		this.key = key;
+	}
+	
+	public double getVisualDisturbanceValue() 
+	{
+		return visualDisturbanceValue;
+	}
+
+	public void setVisualDisturbanceValue(double visualDisturbanceValue) 
+	{
+		this.visualDisturbanceValue = visualDisturbanceValue;
+	}
+	
+	
+
 }
