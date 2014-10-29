57f054128ace568a7acb5f74617d6623e0f0f5e9
kuroitenshi <phibo_pg@yahoo.com>
2014-10-29 11:38:44 +0800

    Flame Percentage Text Writer

--- src/controller/GeckoController.java
+++ src/controller/GeckoController.java
@@ -42,6 +42,7 @@ public class GeckoController
 				File movieFileChosen = new File(filepath);
 				ArrayList<Double> shotVisualDisturbance = new ArrayList<Double>();
 				ArrayList<Double> shotLuminance = new ArrayList<Double>();
+				ArrayList<Double> shotFlamePercentage = new ArrayList<Double>();
 			
 					
 				extractionModel.setMovieFile(movieFileChosen);
@@ -51,40 +52,48 @@ public class GeckoController
 				movieSegmentation.segmentMovie();
 					
 				
-				System.out.println("VISUAL DISTURBANCE VALUES");
+				System.out.println("VISUAL FEATURE VALUES");
 				StringBuilder visualDisturbanceValues = new StringBuilder();
 				StringBuilder visualLuminanceValues = new StringBuilder();
+				StringBuilder visualFlamePercentageValues = new StringBuilder();
 				for(int i = 1; i <= movieSegmentation.getShotRangeNumber(); i++)						
 				{						
 					
 					Shot shot = new Shot(i, extractionModel.getVisualDataPath() + "\\ShotRange.txt", extractionModel.getFramesPath());
 					shotVisualDisturbance.add(shot.computeVisualDisturbance());
 					shotLuminance.add(shot.computeLuminance());
+					shotFlamePercentage.add(shot.computeFlamePercentage());
 					visualDisturbanceValues = visualDisturbanceValues.append("SHOT " + i + " VALUE " + shotVisualDisturbance.get(i-1) + System.lineSeparator());
 					visualLuminanceValues = visualLuminanceValues.append("SHOT " + i + " VALUE " + shotLuminance.get(i-1) + System.lineSeparator());						
+					visualFlamePercentageValues = visualFlamePercentageValues.append("SHOT " + i + " VALUE " + shotFlamePercentage.get(i-1) + System.lineSeparator());
 				}
 					
 				File resultVisualDisturbanceFile = new File(extractionModel.getParentResultPath().concat("\\Visual Data\\Visual Disturbance Values.txt"));
 				File resultLuminanceFile = new File(extractionModel.getParentResultPath().concat("\\Visual Data\\Luminance Values.txt"));
+				File resultFlamePercentageFile = new File(extractionModel.getParentResultPath().concat("\\Visual Data\\Flame Percentage Valuess.txt"));
 			    
 				FileWriter resultVisualDisturbanceWriter = null;
 			    FileWriter resultLuminanceWriter = null;
+			    FileWriter resultFlamePercentageWriter = null;
 			    
 			    try 
 				{								
 					resultVisualDisturbanceWriter = new FileWriter(resultVisualDisturbanceFile.getAbsoluteFile());
 					resultLuminanceWriter = new FileWriter(resultLuminanceFile.getAbsoluteFile());
+					resultFlamePercentageWriter = new FileWriter(resultFlamePercentageFile.getAbsoluteFile());
 					
 				   	BufferedWriter visualDisturbanceBufferedWriter = new BufferedWriter(resultVisualDisturbanceWriter);
-					BufferedWriter luminanceBufferedWriter = new BufferedWriter(resultLuminanceWriter);				    	
+					BufferedWriter luminanceBufferedWriter = new BufferedWriter(resultLuminanceWriter);				
+					BufferedWriter flamePercentageBufferedWriter = new BufferedWriter(resultFlamePercentageWriter);		
 						
 					visualDisturbanceBufferedWriter.write(visualDisturbanceValues.toString());
 			    	visualDisturbanceBufferedWriter.close();
 			    	
-
 				   	luminanceBufferedWriter.write(visualLuminanceValues.toString());
 					luminanceBufferedWriter.close();
 					
+					flamePercentageBufferedWriter.write(visualFlamePercentageValues.toString());
+					flamePercentageBufferedWriter.close();
 				} 
 			    catch (IOException e) 
 				{
