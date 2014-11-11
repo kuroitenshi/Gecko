24b1ec47e27d282cc8517323b922ef5ebd6c488a
kuroitenshi <phibo_pg@yahoo.com>
2014-11-11 23:52:06 +0800

    Praatcon Runtime Execution

--- src/model/AudialFeatures.java
+++ src/model/AudialFeatures.java
@@ -1,6 +1,32 @@
 package model;
 
+import model.Objects.StreamConsumer;
+
 public class AudialFeatures
 {
-	/*Audio compuatations*/
+	
+	public void openPraatConnection()
+	{
+		
+		String command = "praatcon  C:\\Users\\Pheebz\\Desktop\\test.praat";
+		
+		try 
+		{	
+			Runtime runTime = Runtime.getRuntime();
+			Process extractionProcess = runTime.exec(command);									
+			
+			StreamConsumer errorGobbler = new StreamConsumer(extractionProcess.getErrorStream(), "ERROR");            
+			StreamConsumer outputGobbler = new StreamConsumer(extractionProcess.getInputStream(), "OUTPUT");
+			
+	        errorGobbler.start();
+	        outputGobbler.start();
+	        int exitVal = extractionProcess.waitFor();
+            System.out.println("ExitValue: " + exitVal);   
+		} catch (Throwable e) 
+		{			
+			e.printStackTrace();
+		}
+		
+		
+	}
 }
